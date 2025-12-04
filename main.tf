9433b0acc257a4bb9daf5b17ab9e9fa885290792
Afu Tse <solapa@gmail.com>
2025-12-04 16:15:15 -0500

    Add tfstage storage

--- main.tf
+++ main.tf
@@ -1,10 +1,9 @@
 provider "google" {
   project = var.project_id
   region  = var.region
-  credentials = file("~/terraform-key.json")
 }
 
-resource "google_storage_bucket" "mi_primer_bucket" {
+resource "google_storage_bucket" "my_first_bucket" {
   name          = var.bucket_name
   location      = var.region
   force_destroy = true
--- versions.tf
+++ versions.tf
@@ -1,5 +1,11 @@
 terraform {
   required_version = ">= 1.0"
+
+  backend "gcs" {
+    bucket = "bootcamp-478214-tfstate"
+    prefix = "terraform/state"
+  }
+
   required_providers {
     google = {
       source  = "hashicorp/google"
